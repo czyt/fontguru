147c95a74307b42ee2c83c98ba9cbfb1d2f20b54
czyt <czyt@w.cn>
2023-05-15 22:48:57 +0800

    add font cache fresh

--- internal/fontinstall/install_windows.go
+++ internal/fontinstall/install_windows.go
@@ -7,15 +7,31 @@ import (
 	"unsafe"
 )
 
+var (
+	addFontResourceEx = syscall.NewLazyDLL("gdi32.dll").NewProc("AddFontResourceExW")
+	sendMessage       = syscall.NewLazyDLL("user32.dll").NewProc("SendMessageW")
+)
+
 func InstallFont(localPath string) error {
-	var gdi32 = syscall.NewLazyDLL("gdi32.dll")
-	var addFont = gdi32.NewProc("AddFontResourceExW")
-	ret, _, err := addFont.Call(
+	r1, _, err := addFontResourceEx.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(localPath))),
 		0,
 		0,
 	)
-	if ret == 0 {
+	if r1 == 0 {
+		return err
+	}
+	// Reload the font cache
+	hwndBroadcast := uintptr(0xFFFF)
+	wmFontchange := uintptr(0x001D)
+	lparam := uintptr(0)
+	r1, _, err = sendMessage.Call(
+		hwndBroadcast,
+		wmFontchange,
+		0,
+		lparam,
+	)
+	if r1 == 0 {
 		return err
 	}
 	return nil
